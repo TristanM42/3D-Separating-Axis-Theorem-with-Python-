a655519e897af90d8d76a83cc16772ffa0042493
Steve Zhu <704242527zzh@gmail.com>
2019-02-21 14:41:01 -0500

    new

--- SAT.py
+++ SAT.py
@@ -1,3 +1,28 @@
 import numpy as np
+import math
 
-print("new")
+'''
+    Transform the roll pitch yaw to rotation matrix
+'''
+def RPY_to_Rotation(roll, pitch, yaw):
+    yawMatrix = np.matrix([
+        [math.cos(yaw), -math.sin(yaw), 0],
+        [math.sin(yaw), math.cos(yaw), 0],
+        [0, 0, 1]
+    ])
+
+    pitchMatrix = np.matrix([
+        [math.cos(pitch), 0, math.sin(pitch)],
+        [0, 1, 0],
+        [-math.sin(pitch), 0, math.cos(pitch)]
+    ])
+
+    rollMatrix = np.matrix([
+        [1, 0, 0],
+        [0, math.cos(roll), -math.sin(roll)],
+        [0, math.sin(roll), math.cos(roll)]
+    ])
+
+    R = yawMatrix * pitchMatrix * rollMatrix
+    return R
+
