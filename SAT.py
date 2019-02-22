9a7477e04b6515f42f94fe2aa23af7203f2807eb
Steve Zhu <704242527zzh@gmail.com>
2019-02-22 18:05:11 -0500

    new

--- SAT.py
+++ SAT.py
@@ -31,14 +31,12 @@ def RPY_to_Rotation(RPY_list):
     R = yawMatrix * pitchMatrix * rollMatrix
     return R
 
-
 def collide_check(ref_min, ref_max, _min, _max):
     if ref_min>_max or ref_max<_min:
         return False
     return True
 
-
-def collision_detect(ref_dimention, cuboid_corner):
+def collision_detect(ref_corner, cuboid_corner):
     x_min = np.min(cuboid_corner[:, 0])
     x_max = np.max(cuboid_corner[:, 0])
     y_min = np.min(cuboid_corner[:, 1])
@@ -46,12 +44,15 @@ def collision_detect(ref_dimention, cuboid_corner):
     z_min = np.min(cuboid_corner[:, 2])
     z_max = np.max(cuboid_corner[:, 2])
 
-    xref_min = -ref_dimention[0]/2
-    xref_max = ref_dimention[0]/2
-    yref_min = -ref_dimention[1]/2
-    yref_max = ref_dimention[1]/2
-    zref_min = -ref_dimention[2]/2
-    zref_max = ref_dimention[2]/2
+    xref_min = np.min(ref_corner[:, 0])
+    xref_max = np.max(ref_corner[:, 0])
+    yref_min = np.min(ref_corner[:, 1])
+    yref_max = np.max(ref_corner[:, 1])
+    zref_min = np.min(ref_corner[:, 2])
+    zref_max = np.max(ref_corner[:, 2])
+    # print(x_min, x_max, xref_min, xref_max)
+    # print(y_min, y_max, yref_min, yref_max)
+    # print(z_min, z_max, zref_min, zref_max)
 
     x_collide = collide_check(xref_min, xref_max, x_min, x_max)
     y_collide = collide_check(yref_min, yref_max, y_min, y_max)
@@ -61,34 +62,50 @@ def collision_detect(ref_dimention, cuboid_corner):
 
 def InRef(cuboid_ref, cuboid):
     T_matrix = np.array([[1,1,1],[1,-1,1],[-1,-1,1],[-1,1,1],[1,1,-1],[1,-1,-1],[-1,-1,-1],[-1,1,-1]])
+    Projection_matrix = np.array([[1,0,0],[0,1,0],[0,0,1]])
 
-    cuboid_corner_initial = np.array([cuboid["Dimension"][0]/2, cuboid["Dimension"][1]/2, cuboid["Dimension"][2]/2])
-    cuboid_corner_dimension = np.tile(cuboid_corner_initial, (8,1))
-    cuboid_corner = cuboid_corner_dimension * T_matrix
     Rotation_ref = RPY_to_Rotation(cuboid_ref["Orientation"])
     Rotation_cub = RPY_to_Rotation(cuboid["Orientation"])
-    Rotation_cub_to_ref = np.linalg.inv(Rotation_cub) @ Rotation_ref
 
-    cuboid_corner_after_Rotation = Rotation_cub_to_ref @ cuboid_corner.T
+    cuboid_corner_initial = np.array(
+        [cuboid["Dimension"][0] / 2, cuboid["Dimension"][1] / 2, cuboid["Dimension"][2] / 2])
+    cuboid_corner_dimension = np.tile(cuboid_corner_initial, (8, 1))
+    cuboid_corner = cuboid_corner_dimension * T_matrix
 
-    cuboid_center_to_ref = np.array(cuboid["Origin"]) - np.array(cuboid_ref["Origin"])
-    cuboid_corner_new_axis = cuboid_corner_after_Rotation.T + np.tile(cuboid_center_to_ref, (8,1))
-    plot_linear_cube(cuboid_corner_new_axis, color='red')
+    ref_corner_initial = np.array(
+        [cuboid_ref["Dimension"][0] / 2, cuboid_ref["Dimension"][1] / 2, cuboid_ref["Dimension"][2] / 2])
+    ref_corner_dimension = np.tile(ref_corner_initial, (8, 1))
+    ref_corner = ref_corner_dimension * T_matrix
 
-    Collision_or_not = collision_detect(cuboid_ref["Dimension"],cuboid_corner_new_axis)
-    return Collision_or_not
+    ref_corners = ref_corner @ Rotation_ref + np.array(cuboid_ref["Origin"])
+    cub_corners = cuboid_corner @ Rotation_cub + np.array(cuboid["Origin"])
+    # plot_linear_cube(cub_corners, ref_corners, color='red')
 
+    Projection_matrix_ref = Projection_matrix @ Rotation_ref
+    cuboid_corner_new = cub_corners @ Projection_matrix_ref.T
+    ref_corner_new = ref_corners @ Projection_matrix_ref.T
+    # plot_linear_cube(cuboid_corner_new, ref_corner_new, color='red')
+
+    Collision_or_not_ref = collision_detect(ref_corner_new,cuboid_corner_new)
+
+    Projection_matrix_cub = Projection_matrix @ Rotation_cub
+    cuboid_corner_new = cub_corners @ Projection_matrix_cub.T
+    ref_corner_new = ref_corners @ Projection_matrix_cub.T
+
+    Collision_or_not_cub = collision_detect(ref_corner_new,cuboid_corner_new)
+
+
+    return Collision_or_not_ref and Collision_or_not_cub
 
 
 def collosion_detect(cuboid_1,cuboid_2):
     result1 = InRef(cuboid_1, cuboid_2)   #  In reference of cuboid1
-    result2 = InRef(cuboid_2, cuboid_1)   #  In reference of cuboid2
-    return result1 and result2
+    return result1
 
 
 def main():
     cuboid_1 = {"Origin": [0, 0, 0], "Orientation": [0, 0, 0], "Dimension": [2, 2, 2]}
-    cuboid_2 = {"Origin": [2, 2, 2], "Orientation": [0, 0, 0], "Dimension": [2, 2, 2]}
+    cuboid_2 = {"Origin": [1.7, 1.7, 0], "Orientation": [0, 0, np.pi/4], "Dimension": [2, 2, 2]}
     print(collosion_detect(cuboid_1,cuboid_2))
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -4,12 +4,15 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 
-def plot_linear_cube(point_set, color='red'):
+def plot_linear_cube(PS1, PS2, color='red'):
     fig = plt.figure()
     ax = Axes3D(fig)
+
+    point_set = PS1
     xx = point_set[:,0].reshape(-1).tolist()[0]
     yy = point_set[:,1].reshape(-1).tolist()[0]
     zz = point_set[:,2].reshape(-1).tolist()[0]
+
     kwargs = {'alpha': 1, 'color': color}
     ax.plot3D(xx, yy, zz, **kwargs)
     ax.plot3D([point_set[0,0], point_set[3,0]], [point_set[0,1], point_set[3,1]], [point_set[0,2], point_set[3,2]], **kwargs)
@@ -18,10 +21,22 @@ def plot_linear_cube(point_set, color='red'):
     for i in range(4):
         ax.plot3D([point_set[i,0], point_set[i,0]], [point_set[i,1], point_set[i,1]], [point_set[i,2], point_set[4+i,2]], **kwargs)
 
+    point_set = PS2
+    xx = point_set[:,0].reshape(-1).tolist()[0]
+    yy = point_set[:,1].reshape(-1).tolist()[0]
+    zz = point_set[:,2].reshape(-1).tolist()[0]
 
+    kwargs = {'alpha': 2, 'color': 'blue'}
+    ax.plot3D(xx, yy, zz, **kwargs)
+    ax.plot3D([point_set[0, 0], point_set[3, 0]], [point_set[0, 1], point_set[3, 1]],
+              [point_set[0, 2], point_set[3, 2]], **kwargs)
+    ax.plot3D([point_set[4, 0], point_set[7, 0]], [point_set[4, 1], point_set[7, 1]],
+              [point_set[4, 2], point_set[7, 2]], **kwargs)
 
+    for i in range(4):
+        ax.plot3D([point_set[i, 0], point_set[i, 0]], [point_set[i, 1], point_set[i, 1]],
+                  [point_set[i, 2], point_set[4 + i, 2]], **kwargs)
     plt.title('Cube')
     plt.show()
-
 
 
