b42f0813833b6afe0c2a4ed3ce33aa692c136072
Steve Zhu <704242527zzh@gmail.com>
2019-02-22 18:35:00 -0500

    new

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 3D-Separating-Axis-Theorem-with-Python
+
+The input are information for two cubes:
+"Origin": Origin Cordinates
+"Orientation": Roll Pitch Yaw angle relative to x,y,z axis respectively
+"Dimension": width for each side
+
+Run main.py to check the collision between two cubes
+plot_cube.py is used to plot the cube for ease of debugging
+
+More detailed explanation please refer to the comments in main.py
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import numpy as np
 import math
-from test import *
-
-
+from plot_cube import *
 
 
 '''
@@ -31,11 +29,14 @@ def RPY_to_Rotation(RPY_list):
     R = yawMatrix * pitchMatrix * rollMatrix
     return R
 
+# the cubes do not collide with each other in one axis only when the max of one cube is smaller than another cube
+# under the same axis
 def collide_check(ref_min, ref_max, _min, _max):
     if ref_min>_max or ref_max<_min:
         return False
     return True
 
+# Detect collision in each dimension
 def collision_detect(ref_corner, cuboid_corner):
     x_min = np.min(cuboid_corner[:, 0])
     x_max = np.max(cuboid_corner[:, 0])
@@ -50,9 +51,6 @@ def collision_detect(ref_corner, cuboid_corner):
     yref_max = np.max(ref_corner[:, 1])
     zref_min = np.min(ref_corner[:, 2])
     zref_max = np.max(ref_corner[:, 2])
-    # print(x_min, x_max, xref_min, xref_max)
-    # print(y_min, y_max, yref_min, yref_max)
-    # print(z_min, z_max, zref_min, zref_max)
 
     x_collide = collide_check(xref_min, xref_max, x_min, x_max)
     y_collide = collide_check(yref_min, yref_max, y_min, y_max)
@@ -60,48 +58,55 @@ def collision_detect(ref_corner, cuboid_corner):
 
     return (x_collide and y_collide and z_collide)
 
-def InRef(cuboid_ref, cuboid):
+def Check_Collision(cuboid_ref, cuboid):
     T_matrix = np.array([[1,1,1],[1,-1,1],[-1,-1,1],[-1,1,1],[1,1,-1],[1,-1,-1],[-1,-1,-1],[-1,1,-1]])
     Projection_matrix = np.array([[1,0,0],[0,1,0],[0,0,1]])
 
+    # Calculate rotation matrix for both cubes in respect to the base frame
     Rotation_ref = RPY_to_Rotation(cuboid_ref["Orientation"])
     Rotation_cub = RPY_to_Rotation(cuboid["Orientation"])
 
+    # Rotate each corner point relative to cube's center (do not consider the position relative to base frame's origin)
     cuboid_corner_initial = np.array(
         [cuboid["Dimension"][0] / 2, cuboid["Dimension"][1] / 2, cuboid["Dimension"][2] / 2])
     cuboid_corner_dimension = np.tile(cuboid_corner_initial, (8, 1))
     cuboid_corner = cuboid_corner_dimension * T_matrix
 
+
+    # Rotate each corner point relative to cube's center (do not consider the position relative to base frame's origin)
     ref_corner_initial = np.array(
         [cuboid_ref["Dimension"][0] / 2, cuboid_ref["Dimension"][1] / 2, cuboid_ref["Dimension"][2] / 2])
     ref_corner_dimension = np.tile(ref_corner_initial, (8, 1))
     ref_corner = ref_corner_dimension * T_matrix
 
+    # Add origin to get the absolute cordinates of each corner point
     ref_corners = ref_corner @ Rotation_ref + np.array(cuboid_ref["Origin"])
     cub_corners = cuboid_corner @ Rotation_cub + np.array(cuboid["Origin"])
+    # Uncomment below to plot current position of two cubes
     # plot_linear_cube(cub_corners, ref_corners, color='red')
 
+    #Project each cube onto the normals of reference cube
     Projection_matrix_ref = Projection_matrix @ Rotation_ref
     cuboid_corner_new = cub_corners @ Projection_matrix_ref.T
     ref_corner_new = ref_corners @ Projection_matrix_ref.T
+    Collision_or_not_ref = collision_detect(ref_corner_new,cuboid_corner_new) # check collision
+    # Uncomment below to plot current position of two cubes (in cordinates of the reference cube)
     # plot_linear_cube(cuboid_corner_new, ref_corner_new, color='red')
 
-    Collision_or_not_ref = collision_detect(ref_corner_new,cuboid_corner_new)
-
+    #Project each cube onto the normals of reference cube
     Projection_matrix_cub = Projection_matrix @ Rotation_cub
     cuboid_corner_new = cub_corners @ Projection_matrix_cub.T
     ref_corner_new = ref_corners @ Projection_matrix_cub.T
-
     Collision_or_not_cub = collision_detect(ref_corner_new,cuboid_corner_new)
-
+    # Uncomment below to plot current position of two cubes (in cordinates of the reference cube)
+    # plot_linear_cube(cuboid_corner_new, ref_corner_new, color='red')
 
     return Collision_or_not_ref and Collision_or_not_cub
 
 
 def collosion_detect(cuboid_1,cuboid_2):
-    result1 = InRef(cuboid_1, cuboid_2)   #  In reference of cuboid1
-    return result1
-
+    result = Check_Collision(cuboid_1, cuboid_2)   #  In reference of cuboid1
+    return result
 
 def main():
     cuboid_1 = {"Origin": [0, 0, 0], "Orientation": [0, 0, 0], "Dimension": [2, 2, 2]}
